2eb37dc189ebd3244c35436befb79beb586088ea
Caoanlong <caoanlong@gmail.com>
2018-12-29 18:33:54 +0800

    optimize

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,4 @@
-const SORTS = ['WCG', 'WOS', 'MDP','DRT','MAT','MTR','ALLN','BTC','XRP','ETH','LTC','BCH', 'DASH', 'ENX', 'USDTK']
+const SORTS = ['WCG', 'WOS', 'MDP','DRT','MAT','MTR','ALLN','BTC','XRP','ETH','LTC','BCH', 'DASH', 'ENX', 'USDTK', 'TRX']
 
 const APPID = 'wx9738e5de5c851bbf'
 
